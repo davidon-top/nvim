59f5952492d79b64e11827208be36742bb5d4872
davidon-top <davidkozakke@gmail.com>
2023-05-13 12:43:19 +0200

    finaly cursorline

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -383,6 +383,27 @@ return require("packer").startup(function(use)
       }
 
     -- lenguage specific
+    -- csv
+    use {
+        'cameron-wags/rainbow_csv.nvim',
+        config = function()
+            require 'rainbow_csv'.setup()
+        end,
+        -- optional lazy-loading below
+        -- module = {
+        --     'rainbow_csv',
+        --     'rainbow_csv.fns'
+        -- },
+        -- ft = {
+        --     'csv',
+        --     'tsv',
+        --     'csv_semicolon',
+        --     'csv_whitespace',
+        --     'csv_pipe',
+        --     'rfc_csv',
+        --     'rfc_semicolon'
+        -- }
+    }
     -- rust
     use {"racer-rust/vim-racer",
         config = function ()
--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,5 +1,6 @@
 -- numbers
 vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- tab && indent
@@ -29,13 +30,22 @@ vim.o.smartcase = true
 vim.opt.termguicolors = true
 vim.cmd("colorscheme catppuccin")
 
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.cmd[[
+highlight CursorColumn guibg=none ctermbg=none
+highlight link CursorColumn CursorLine
+]]
+
 -- misc
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.o.mouse = "a"
+vim.opt.showmode = false
 
 vim.o.completeopt = "menuone,noselect"
